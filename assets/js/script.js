deeddd3d3f6e2eeb18e92f313a4a74f80d276b20
jackiem2 <jacqueline.merlan@gmail.com>
2022-01-09 19:17:42 -0600

    added time-block function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,23 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do, YYYY"));
 
 // Time block
+function timeBlock() {
+    var hour = moment().hours();
 
+    $(".time-block").each(function(){
+        var currentHour = parseInt($(this).attr("id"));
+
+        if (currentHour > hour) {
+            $(this).addClass("future");
+        } else if (currentHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+
+
+
+timeBlock();
